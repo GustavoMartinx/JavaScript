b25404893595014dfa3ba5a9aa1d6e313cb3cb1e
Gustavo_Martins <gtavomartins893@gmail.com>
2022-07-14 21:12:30 -0300

    added array and dictionary in data_structs

---
+++ logic/data_structs/dictionary.js
@@ -0,0 +1,8 @@
+// Dictionary
+var fruit = {name: "apple", color:"red"}
+console.log(fruit.name);
+console.log(fruit.color);
+
+// Array of Dictionaries
+var Fruits = [{name:"grape", color:"purple"}, {name:"apple", color:"red"}]
+console.log(Fruits[0].name);
