075f61182376bd4a00c8df4bbb1554e83aee4030
Gustavo_Martins <gtavomartins893@gmail.com>
2022-07-14 21:03:30 -0300

    added conditionals and loops

---
+++ logic/conditionals.js
@@ -0,0 +1,8 @@
+// Condicionais (IF ELSE) + Prompt (scanf)
+var idade = prompt("Qual é a sua idade? ");
+
+if (idade >= 18) {
+    alert("Maior de idade.")
+} else {
+    alert("Menor de idade.")
+}
---
+++ logic/loops.js
@@ -0,0 +1,13 @@
+// Repetition Loops
+
+var count = 0;
+
+while(count < 5) {
+    console.log(count);
+    count++;
+}
+
+
+for(i = 0; i <= 10; i++) {
+    console.log(i);
+}
