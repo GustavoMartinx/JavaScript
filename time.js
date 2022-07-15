15805e71d71ff9f4f43f9b3783155e47d47dee1a
Gustavo_Martins <gtavomartins893@gmail.com>
2022-07-14 21:53:28 -0300

    added time

---
+++ time.js
@@ -0,0 +1,25 @@
+// Working with Time/Dates
+var d = new Date();
+
+// access the date and hour complete (day, month, year, hour, min, sec)
+// acessa a data e hora completa (dia, mes, ano, hora, min, seg)
+alert(d);
+
+// return the month (starting counting from zero, then + 1 )
+// devolve o mes (começando a contagem a partir do zero, então + 1)
+alert(d.getMonth() + 1);
+
+// return the day
+// retorna o dia
+alert(d.getDay());
+
+// access the hour
+// acessa a hora
+alert(d.getHours());
+
+// return the minutes
+// devolve os minutos
+alert(d.getMinutes());
+
+// among many other functions available...
+// entre várias outras funções disponíveis...
